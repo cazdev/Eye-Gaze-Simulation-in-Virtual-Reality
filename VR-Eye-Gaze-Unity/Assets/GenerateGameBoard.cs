cf5ea498d12633ff9979ba3fff25cbd4413546e9
cazdev <chaz6cl@live.com>
2022-02-06 17:26:58 +1100

    Generated game board

--- VR-Eye-Gaze-Unity/Assets/GenerateGameBoard.cs
+++ VR-Eye-Gaze-Unity/Assets/GenerateGameBoard.cs
@@ -6,6 +6,7 @@ public class GenerateGameBoard : MonoBehaviour
 {
     public GameObject TileSquare1;
     public GameObject TileSquare2;
+    public GameObject PlacementSquare;
     public int Columns = 15;
     public int Rows = 15;
     public Transform StartPos;
@@ -18,12 +19,50 @@ public class GenerateGameBoard : MonoBehaviour
 
     void CreateGameBoard()
     {
+        bool isAlternateTile = false;
+        for (int column = 0; column < Columns + 1; column++)
+        {
+            for (int row = 0; row < Rows + 1; row++)
+            {
 
+                // Corner Placement zones
+                Instantiate(
+                        PlacementSquare,
+                        new Vector3(row * 0.03f, 0, column * 0.03f) + StartPos.position - new Vector3(0.015f, -0.01f, 0.015f),
+                        Quaternion.identity,
+                        transform
+                        );
+
+                // Tiles
+                if (column < Columns && row < Rows)
+                {
+                    if (isAlternateTile)
+                    {
+                        Instantiate(
+                            TileSquare1,
+                            new Vector3(row * 0.03f, 0, column * 0.03f) + StartPos.position,
+                            Quaternion.identity,
+                            transform
+                            );
+                    }
+                    else
+                    {
+                        Instantiate(
+                            TileSquare2,
+                            new Vector3(row * 0.03f, 0, column * 0.03f) + StartPos.position,
+                            Quaternion.identity,
+                            transform
+                            );
+                    }
+                    isAlternateTile = !isAlternateTile;
+                }
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
