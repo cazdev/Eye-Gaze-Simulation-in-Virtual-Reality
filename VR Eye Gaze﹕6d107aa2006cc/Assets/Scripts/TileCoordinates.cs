5be49cc0a3cd3ee3b90e6956f579dfb25476b0d1
cazdev <chaz6cl@live.com>
2022-04-03 13:48:02 +1000

    Removed old instance

--- VR Eye Gaze﹕6d107aa2006cc/Assets/Scripts/TileCoordinates.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TileCoordinates : MonoBehaviour
-{
-    public int Row = 0;
-    public int Column = 0;
-}
